bf2729b975a3e7fc8ca57089071dc24b44b1bd63
HINGANE <NDH00877@Nmcorp.Nissan.Biz>
2023-01-06 18:00:10 +0530

    Structure of backend created

---
+++ IB10_AssetManagementSystem/src/main/java/com/nissan/Ib10AssetManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.nissan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ib10AssetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ib10AssetManagementSystemApplication.class, args);
+		System.out.println("Asset Management System - IB10 is running...");
+	}
+
+}
---
+++ IB10_AssetManagementSystem/src/test/java/com/nissan/Ib10AssetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nissan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ib10AssetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
