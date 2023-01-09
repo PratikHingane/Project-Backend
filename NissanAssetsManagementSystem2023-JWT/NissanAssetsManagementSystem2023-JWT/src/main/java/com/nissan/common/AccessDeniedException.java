2e8fde7901a548aa81f99ed40bf2b6ec4b2dddc3
PAWAR <NDH00879@Nmcorp.Nissan.Biz>
2023-01-09 10:31:55 +0530

    Vendor Push

---
+++ NissanAssetsManagementSystem2023-JWT/NissanAssetsManagementSystem2023-JWT/src/main/java/com/nissan/common/AccessDeniedException.java
@@ -0,0 +1,10 @@
+package com.nissan.common;
+
+public class AccessDeniedException extends RuntimeException { //User-Defined 
+
+	public AccessDeniedException(String message) {
+		super(message);
+	}
+	
+
+}
